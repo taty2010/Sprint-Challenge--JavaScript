8fc14a03db770715f40e4d07444ef0fa8521c1bd
taty2010 <shydalys123@gmail.com>
2019-08-17 12:54:38 -0500

    created first higher order function and started callbacks

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,12 +7,27 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume (param1, param2, call){
+  return call(param1, param2);
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+ const add = function(num1, num2){
+
+ }
+
+ const multiply = function(num1, num2){
+   
+}
+
+const greeting = function(num1, num2){
+   
+}
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
