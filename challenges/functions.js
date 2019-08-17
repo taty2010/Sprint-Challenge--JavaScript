629b45047ea2dd78f03899a2ced6c0be959a410f
taty2010 <shydalys123@gmail.com>
2019-08-17 13:03:28 -0500

    added console.log instead of return

--- challenges/functions.js
+++ challenges/functions.js
@@ -12,28 +12,29 @@ function consume (param1, param2, call){
 }
 
 
+
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
  const add = function(num1, num2){
-
+    console.log(num1 + num2);
  }
 
  const multiply = function(num1, num2){
-   
+   console.log( num1 * num2);
 }
 
-const greeting = function(num1, num2){
-   
+const greeting = function(firstName, lastName){
+   console.log( `Hello ${firstName} ${lastName}, nice to meet you!`)
 }
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
